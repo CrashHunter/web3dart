104f1b75d71949b9f3914dd5072119862f7bcc3b
CrashHunter <eclowang@gmail.com>
2022-05-24 15:39:45 +0800

    添加chainid参数

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,14 +2,17 @@ import 'package:http/http.dart';
 import 'package:web3dart/web3dart.dart';
 
 const String privateKey =
-    'f7e5913cf852c8ff7be4dfa2560dacbbba56ce361d41af11efe8360e7ac414f1';
-const String rpcUrl = 'http://localhost:7545';
+    '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+const String rpcUrl = 'http://localhost:8545';
 
 Future<void> main() async {
   // start a client we can use to send transactions
   final client = Web3Client(rpcUrl, Client());
   client.getNetworkId().then((id) {
     print('Network id: $id');
+  });
+  client.getChainId().then((id) {
+    print('Chain id: $id');
   });
 
   final credentials = EthPrivateKey.fromHex(privateKey);
@@ -21,11 +24,12 @@ Future<void> main() async {
   await client.sendTransaction(
     credentials,
     Transaction(
-      to: EthereumAddress.fromHex('0xD295C6e98Af937A32C84219eA1D30c127C0b34a5'),
-      gasPrice: EtherAmount.inWei(BigInt.one),
+      to: EthereumAddress.fromHex('0x70997970C51812dc3A010C7d01b50e0d17dc79C8'),
+      gasPrice: EtherAmount.inWei(BigInt.from(875000000)),
       maxGas: 100000,
       value: EtherAmount.fromUnitAndValue(EtherUnit.ether, 1),
     ),
+    chainId: 31337,
   );
 
   await client.dispose();
